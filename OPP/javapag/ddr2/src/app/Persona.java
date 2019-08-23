684cd8c496ba956232206495534a917056035658
ecaroa <elianacarolinaa@gmail.com>
2019-08-23 11:36:19 -0300

    ..

--- OPP/javapag/ddr2/src/app/Persona.java
+++ OPP/javapag/ddr2/src/app/Persona.java
@@ -9,46 +9,198 @@ public class Persona {
 
     private String nombre;
     private int edad;
-    private int dni;
+    private String DNI;
     private double peso;
     private double altura;
-    private char m;
+    private char sexo;
+ 
+ //Constantes
+    /**
+     * Sexo por defecto
+     */
+    private final static char SEXO_DEF = 'H';
+    /**
+     * El peso de la persona esta por debajo del peso ideal
+     */
+    public static final int INFRAPESO = -1;
+    /**
+     * El peso de la persona esta en su peso ideal
+     */
+    public static final int PESO_IDEAL = 0;
+    /**
+     * El peso de la persona esta por encima del peso ideal
+     */
+    public static final int SOBREPESO = 1;
+ 
 
+//Contructores
+    /**
+     * Constructor por defecto
+     */
     public Persona() {
-        this.nombre = "Lucia";
-        this.edad = 20;
-        this.dni = 39444243;
-        this.peso = 55.32;
-        this.altura = 1.60;
-        this.m = 'm';
+        this("", 0, SEXO_DEF, 0, 0);
     }
-
-    public Persona(String nombre, int edad, char m) {
+    /**
+     * Constructor con 3 parametroe
+     *
+     * @param nombre de la persona
+     * @param edad de la persona
+     * @param sexo de la persona
+     */
+    public Persona(String nombre, int edad, char sexo) {
+        this(nombre, edad, sexo, 0, 0);
+    }
+ 
+    /**
+     * Constructor con 5 parametros
+     *
+     * @param nombre de la persona
+     * @param edad de la persona
+     * @param sexo de la persona
+     * @param peso de la persona
+     * @param altura de la persona
+     */
+    public Persona(String nombre, int edad, char sexo, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
-        this.dni = 39449400;
-        this.peso = 60.10;
-        this.altura = 1.70;
-        this.m = m;
-    }
-
-    public Persona(String nombre, int edad, int dni, double peso, double altura, char m) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.dni = dni;
         this.peso = peso;
         this.altura = altura;
-        this.m = m;
+        generarDni();
+        this.sexo = sexo;
+        comprobarSexo();
     }
-
-    public int calcularIMC(){
-        double pesoActual = peso /
-
+ 
+    //Métodos privados
+    private void comprobarSexo() {
+ 
+        //Si el sexo no es una H o una M, por defecto es H
+        if (sexo != 'H' && sexo != 'M') {
+            this.sexo = SEXO_DEF;
+        }
     }
-
-
-    
-    
-
-
+ 
+    private void generarDni() {
+        final int divisor = 23;
+ 
+        //Generamos un número de 8 digitos
+        int numDNI = ((int) Math.floor(Math.random() * (100000000 - 10000000) + 10000000));
+        int res = numDNI - (numDNI / divisor * divisor);
+ 
+        //Calculamos la letra del DNI
+        char letraDNI = generaLetraDNI(res);
+ 
+        //Pasamos el DNI a String
+        DNI = Integer.toString(numDNI) + letraDNI;
+    }
+ 
+    private char generaLetraDNI(int res) {
+        char letras[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y',
+            'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z',
+            'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+ 
+        return letras[res];
+    }
+ 
+    //Métodos publicos
+    /**
+     * Modifica el nombre de la persona
+     *
+     * @param nombre a cambiar
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    /**
+     * Modifica la edad de la persona
+     *
+     * @param edad a cambiar
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+ 
+    /**
+     * Modifica el sexo de la persona, comprueba que es correcto
+     *
+     * @param sexo a cambiar
+     */
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+ 
+    /**
+     * Modifica el peso de la persona
+     *
+     * @param peso a cambiar
+     */
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+ 
+    /**
+     * Modifica la altura de la persona
+     *
+     * @param altura a cambiar
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+ 
+    /**
+     * Calcula el indice de masa corporal
+     *
+     * @return codigo numerico
+     * <ul><li>-1: la persona esta por debajo de su peso ideal</li>
+     * <li>0: la persona esta en su peso ideal</li>
+     * <li>1: la persona esta por encima de su peso ideal</li></ul>
+     */
+    public int calcularIMC() {
+        //Calculamos el peso de la persona
+        double pesoActual = peso / (Math.pow(altura, 2));
+        //Segun el peso, devuelve un codigo
+        if (pesoActual >= 20 && pesoActual <= 25) {
+            return PESO_IDEAL;
+        } else if (pesoActual < 20) {
+            return INFRAPESO;
+        } else {
+            return SOBREPESO;
+        }
+    }
+ 
+    /**
+     * Indica si la persona es mayor de edad
+     *
+     * @return true si es mayor de edad y false es menor de edad
+     */
+    public boolean esMayorDeEdad() {
+        boolean mayor = false;
+        if (edad >= 18) {
+            mayor = true;
+        }
+        return mayor;
+    }
+ 
+    /**
+     * Devuelve informacion del objeto
+     *
+     * @return cadena con toda la informacion
+     */
+    @Override
+    public String toString() {
+        String sexo;
+        if (this.sexo == 'H') {
+            sexo = "hombre";
+        } else {
+            sexo = "mujer";
+        }
+        return "Informacion de la persona:\n"
+                + "Nombre: " + nombre + "\n"
+                + "Sexo: " + sexo + "\n"
+                + "Edad: " + edad + " años\n"
+                + "DNI: " + DNI + "\n"
+                + "Peso: " + peso + " kg\n"
+                + "Altura: " + altura + " metros\n";
+    }
+ 
 }
